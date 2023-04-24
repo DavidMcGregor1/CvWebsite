ff5d904923f80fe9f3a1410dcd1b9493dab9a4c9
David.McGregor <David.McGregor@hl.co.uk>
2023-04-24 17:09:56 +0100

    added a pop up for the epq section that works on mobile

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 console.log("WOrking");
 
+// <!---------- About Me ---------!>
+
 var aboutMeNavLink = document.getElementById("aboutMeNavLink");
 
 if (window.matchMedia("(max-width: 617px)").matches) {
@@ -9,4 +11,29 @@ if (window.matchMedia("(max-width: 617px)").matches) {
 } else {
   aboutMeNavLink.href = "#Workouts";
   console.log("workouts -> " + aboutMeNavLink.href);
+}
+
+// <!---------- Qualifications ---------!>
+
+var qualificationsNavLink = document.getElementById("qualificationsNavLink");
+
+if (window.matchMedia("(max-width: 617px)").matches) {
+  console.log("under 524px");
+  qualificationsNavLink.href = "#qualificationsMobile";
+  console.log("mobile -> " + qualificationsNavLink.href);
+} else {
+  qualificationsNavLink.href = "#Statistics";
+  console.log("workouts -> " + qualificationsNavLink.href);
+}
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+  document.getElementById("epqMobileText").style.display = "none";
+  document.getElementById("epqOpenButton").style.display = "none";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+  document.getElementById("epqMobileText").style.display = "block";
+  document.getElementById("epqOpenButton").style.display = "block";
 }
