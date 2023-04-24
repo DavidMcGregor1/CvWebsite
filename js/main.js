23134650ddec99033674f375d032a2912b4a6fb9
David.McGregor <David.McGregor@hl.co.uk>
2023-04-24 15:18:52 +0100

    added a media query and javascript to hide the about section table and replace it with a smaller piece of text when it is in mobile mode

---
+++ js/main.js
@@ -0,0 +1,12 @@
+console.log("WOrking");
+
+var aboutMeNavLink = document.getElementById("aboutMeNavLink");
+
+if (window.matchMedia("(max-width: 524px)").matches) {
+  console.log("under 524px");
+  aboutMeNavLink.href = "#aboutMeMobile";
+  console.log("mobile -> " + aboutMeNavLink.href);
+} else {
+  aboutMeNavLink.href = "#Workouts";
+  console.log("workouts -> " + aboutMeNavLink.href);
+}
