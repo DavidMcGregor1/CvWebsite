040dd1f9f8831b81b18f5bd18e296ab19199fb0f
David.McGregor <David.McGregor@hl.co.uk>
2023-04-24 15:43:03 +0100

    added the text for the mobile version of the about section, made the media query pixels slightly bigger

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ console.log("WOrking");
 
 var aboutMeNavLink = document.getElementById("aboutMeNavLink");
 
-if (window.matchMedia("(max-width: 524px)").matches) {
+if (window.matchMedia("(max-width: 617px)").matches) {
   console.log("under 524px");
   aboutMeNavLink.href = "#aboutMeMobile";
   console.log("mobile -> " + aboutMeNavLink.href);
